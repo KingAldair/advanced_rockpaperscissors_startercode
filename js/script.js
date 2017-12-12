dfb32be4284f739b9bac4d1a5b855cbb6726f574
Aldair <garcia3@citypolyhs.org>
2017-12-12 23:25:20 +0000

    I made is and els

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
 /*global $*/
+
+
 //Class 1:
         // Fork and clone the repo and set up your project workspace and link files
         // Write HTML for your project
@@ -17,28 +19,55 @@
 
 // *************************** YOUR CODE BELOW *******************************
 //******************TEST EARLY AND OFTEN USING console.log() ******************
-//****************** SERIOUSLY TEST USING console.log()!!! ******************
-
+//****************** SERIOUSLY TEST USING console.log()!!! ********************     
 //GLOBAL VARIABLES
-var userChoice="";
-console.log()
-
-var computerChoice="";
-console.log()
-
-var winner="";
-console.log()
 
 var choice=["Rock","Paper","Scissors"];
-console.log()
+
+var userChoice;
+
+userChoice=($("#input").val());
+
+console.log();
+
+var computerChoice;
+computerChoice=choice[Math.floor(Math.random()*choice.length)];
+
+console.log();
+
+var winner="";
+console.log();
+
+//"https://render.fineartamerica.com/images/rendered/medium/greeting-card/images-medium-5/fist-made-of-rock-anthony-doudt.jpg" 
+//"https://i.ytimg.com/vi/Zr2S9rrAHbY/maxresdefault.jpg"
+//"https://previews.123rf.com/images/denisnata/denisnata1209/denisnata120900373/15334412-Woman-hand-with-scissors-isolated-on-white-background-Stock-Photo.jpg"
+
 
 //FUNCTIONS
+$("#shoot").click(function(){
+    $("#mechoice").html($("#input").val());
+});
 
-$("#shoot").append(function()
+$("#shoot").click(function(){
+    if ($("#input").val() === "rock","Rock","ROckSSS"){
+    $(".me").attr("src","https://render.fineartamerica.com/images/rendered/medium/greeting-card/images-medium-5/fist-made-of-rock-anthony-doudt.jpg");
+    }
+});
 
- $(".me").
 
-)
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
